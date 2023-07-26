3a9c0d3f0e9ba379fe3cb422c6516e0479433f16
Erik Widman <104375679+erikwidman@users.noreply.github.com>
2023-07-26 13:52:16 -0500

    Add files via upload

---
+++ chat_with_data/credentials.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Secret credentials for OpenAI LLM model
+
+llm_api_key = "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"
+
+weather_api_key = "578a4f1e169be8dab72f775d9cebe2e0"
+
+news_api_key = "8a940557f06e4a31829ac7cde518d1e9"
+
+serpapi_api_key = "a50edebef0e427ca720ce75302aa688e409fffd008055d49bbc72cb00fe605fe"
